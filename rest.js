36b635c3d6e2fc9afaa3a9eec42847ae771163fd
thanhnew2001 <69960815+thanhnew2001@users.noreply.github.com>
2021-11-16 09:39:02 +0700

    add client to monitor message broadcast

--- rest.js
+++ rest.js
@@ -3,16 +3,20 @@ var app = express();
 
 const WebSocket = require('ws');
 
+const path = require('path')
+
+app.use('/frontend', express.static(path.join(__dirname, 'frontend')))
+
 app.get('/notification', async function (req, res) {
 
     var query = require('url').parse(req.url,true).query;
 
-    const connection = new WebSocket('ws://localhost:8080');
+    const connection = new WebSocket('ws://localhost:9000');
     connection.binaryType = "arraybuffer"
 
     connection.onopen = () => {
         console.log('connected');
-        connection.send(query.message + new Date());
+        connection.send(query.message);
     };
 
     res.send(query.message)
